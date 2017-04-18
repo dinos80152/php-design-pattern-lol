644ce3959f02564274871a12ab5ff1a4599177d9
DinoLai <dinos80152@gmail.com>
2017-04-19 01:35:44 +0800

    add factory and strategy pattern example

---
+++ strategy.php
@@ -0,0 +1,77 @@
+<?php
+
+interface AttackRange
+{
+    public function attackRange();
+}
+
+class NonAttackRange implements AttackRange
+{
+    public function attackRange() {
+        echo "I can't attack\n";
+    }
+}
+
+class ShortAttackRange implements AttackRange
+{
+    public function attackRange() {
+        echo "attack short range\n";
+    }
+}
+
+class LongAttackRange implements AttackRange
+{
+    public function attackRange() {
+        echo "attack long range\n";
+    }
+}
+
+class Legend
+{
+
+    private $attackRange;
+
+    public function __construct(AttackRange $attackRange) {
+        $this->attackRange = $attackRange;
+    }
+
+    public function performAttackRange() {
+        $this->attackRange->attackRange();
+    }
+
+    public function setAttackRange(AttackRange $attackRange) {
+        $this->attackRange = $attackRange;
+    }
+}
+
+class Nidalee extends Legend
+{
+    private $mode;
+
+    public function __construct() {
+        parent::__construct(new LongAttackRange);
+        $this->mode = "human";
+    }
+
+    public function transform () {
+        if ($this->mode === "human") {
+            $this->mode = "jaguar";
+        } else {
+            $this->mode = "human";
+        }
+        echo "Mode:" . $this->mode . "\n";
+    }
+}
+
+// Initialize Nidalee as Human Mode
+$nidalee = new Nidalee();
+$nidalee->performAttackRange();
+
+// Press R to transform to Jaguar mode
+$nidalee->transform();
+$nidalee->setAttackRange(new ShortAttackRange);
+$nidalee->performAttackRange();
+
+// Got Teemo Q
+$nidalee->setAttackRange(new NonAttackRange);
+$nidalee->performAttackRange();
