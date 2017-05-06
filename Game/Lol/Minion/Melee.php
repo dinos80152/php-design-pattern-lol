5813d323b6311e60c1f5869d68839c586d62a069
dinos80152 <dinos80152@gmail.com>
2017-05-06 20:40:48 +0800

    strengthen minion package

--- Game/Lol/Minion/Melee.php
+++ Game/Lol/Minion/Melee.php
@@ -4,9 +4,9 @@ namespace Game\Lol\Minion;
 
 class Melee extends Minion
 {
-
     public function __construct()
     {
+        parent::__construct(450);
         echo "Melee Born";
         echo PHP_EOL;
     }
--- Game/Lol/Minion/Minion.php
+++ Game/Lol/Minion/Minion.php
@@ -3,5 +3,20 @@ namespace Game\Lol\Minion;
 
 abstract class Minion
 {
+    private $health;
 
+    public function __construct($health)
+    {
+        $this->health = $health;
+    }
+
+    public function injure($damage)
+    {
+        $this->health = $this->health - $damage;
+    }
+
+    public function getHealth()
+    {
+        return $this->health;
+    }
 }
